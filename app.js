8773357b46fd020214e7d0a5c205412f1779c44c
lucasuirinolopes <lucas.quirino.lopes@escola.pr.gov.br>
2025-09-08 08:57:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+// Seleciona o elemento flashcard
+const flashcard = document.querySelector('.flashcard');
 
+// Adiciona um "ouvinte" de evento de clique
+flashcard.addEventListener('click', () => {
+    // Alterna a classe 'flip' no flashcard
+    flashcard.classList.toggle('flip');
+});
